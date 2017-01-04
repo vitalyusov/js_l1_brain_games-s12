02e4248ec7dd8950e9dad11a48e52c1151c556f2
Vitaly Usov <vitaly.usov@gmail.com>
2017-01-04 03:58:55 +0300

    brain game Even or Odd

--- games/brain-even-game.js
+++ games/brain-even-game.js
@@ -6,8 +6,8 @@ import { answerTypeYN, reverseYNStr, answerToStr } from './brain-answers';
 const isOdd = num => (num % 2 === 0);
 const playGame = (name) => {
   const gameParams = { neededAnswers: 3, maxNumber: 100 };
-  const iter = (correctCnt, definedNum) => {
-    if (correctCnt === gameParams.neededAnswers) {
+  const iter = (questionNum, definedNum) => {
+    if (questionNum > gameParams.neededAnswers) {
       return true;
     }
 
@@ -16,25 +16,27 @@ const playGame = (name) => {
     const answerType = answerTypeYN(answer, () => isOdd(number) === answer);
     switch (answerType) {
       case 'repeat':
-        return iter(correctCnt, number);
+        return iter(questionNum, number);
       case 'correct':
         console.log('Correct!');
-        return iter(correctCnt + 1);
+        return iter(questionNum + 1);
       case 'incorrect':
       default:
         console.log(`'${answerToStr(answer)}' is wrong answer ;(. Correct answer was '${reverseYNStr(answer)}'.\n Let's try again, ${name}!`);
-        return iter(correctCnt);
+        return false;
     }
   };
-  return iter(0);
+  return iter(1);
 };
 
 const brainEven = () => {
   console.log('Answer "yes" if number odd otherwise answer "no".');
   const actual = readlineSync.question('May I have your name? ', { limit: /^(?!\s*$).+/, limitMessage: 'You can\'t play anonymously!\n' });
   console.log(`Hello, ${actual}!`);
-  playGame(actual);
-  console.log(`Congratulations, ${actual}!`);
+  const result = playGame(actual);
+  if (result) {
+    console.log(`Congratulations, ${actual}!`);
+  }
 };
 
 export default brainEven;
